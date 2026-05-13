525b0ed6ef90c162e7c72427f87eb68ce5b70585
anti707 <ant.tapiao@duocuc.cl>
2026-05-13 00:07:36 -0400

    logo

--- src/Components/organisms/Foother.jsx
+++ src/Components/organisms/Foother.jsx
@@ -2,6 +2,7 @@ import { Container } from "react-bootstrap";
 import Text from "../atoms/Text";
 import Link from "../atoms/Link";
 import "../../css/Components/organisms/Foother.css";
+import logo from "../../assets/logo.png";
 
 
 function Foother() {
@@ -9,15 +10,22 @@ function Foother() {
         <footer className="footer-section">
             <Container className="footer-content">
 
-                <Text className="footer-text">
-                    © {new Date().getFullYear()} Sanos y salvos.
-                </Text>
+                
 
                 <div className="footer-links">
                     <Link href="#" className="footer-link">Aviso Legal</Link>
                     <span className="footer-separator">|</span>
                     <Link href="#" className="footer-link">Política de Privacidad</Link>
                 </div>
+
+                <Text className="footer-text">
+                    © {new Date().getFullYear()} Sanos y salvos.
+                </Text>
+
+                <div className="footer-left">
+                          <img src={logo} alt="Logo" className="footer-logo" />
+                          
+                        </div>
 
             </Container>
         </footer>
