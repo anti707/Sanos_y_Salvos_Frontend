fae189f466faf73606b3e217d6dcc1edc9a4c748
anti707 <ant.tapiao@duocuc.cl>
2026-05-15 17:07:43 -0400

    bootstrap js

--- src/Components/molecules/MenuDesplegable.jsx
+++ src/Components/molecules/MenuDesplegable.jsx
@@ -2,18 +2,18 @@
 
 function MenuDesplegable({}) {
   return (
-   <div className="dropdown">
-  <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-    Menu
-  </a>
+    <div className="dropdown">
+    <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+        Menu
+    </a>
 
-  <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <li><a className="dropdown-item" href="#">Mi perfil</a></li>
-    <li><a className="dropdown-item" href="#">Contactanos</a></li>
-    <li><a className="dropdown-item" href="#">Configuracion</a></li>
-  </ul>
-</div>
-  );
+    <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+        <li><a className="dropdown-item" href="#">Mi perfil</a></li>
+        <li><a className="dropdown-item" href="#">Contactanos</a></li>
+        <li><a className="dropdown-item" href="#">Configuracion</a></li>
+    </ul>
+    </div>
+    );
 }
 
 export default MenuDesplegable;
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import './css/global.css';
 import "leaflet/dist/leaflet.css";
 
