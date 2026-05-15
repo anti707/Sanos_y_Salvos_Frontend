a41f734ce6258c67c8e54d14aad4b4faa8a5f75b
anti707 <ant.tapiao@duocuc.cl>
2026-05-15 17:30:37 -0400

    intento decoracion

--- src/Components/molecules/MenuDesplegable.jsx
+++ src/Components/molecules/MenuDesplegable.jsx
@@ -1,19 +1,41 @@
 
-
-function MenuDesplegable({}) {
+function MenuDesplegable() {
   return (
     <div className="dropdown">
-    <a className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
-        Menu
-    </a>
+      <button
+        className="btn btn-dark dropdown-toggle px-4 py-2 rounded-pill shadow"
+        type="button"
+        id="dropdownMenuButton"
+        data-bs-toggle="dropdown"
+        aria-expanded="false"
+      >
+        ☰ Menú
+      </button>
 
-    <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-        <li><a className="dropdown-item" href="#">Mi perfil</a></li>
-        <li><a className="dropdown-item" href="#">Contactanos</a></li>
-        <li><a className="dropdown-item" href="#">Configuracion</a></li>
-    </ul>
+      <ul
+        className="dropdown-menu dropdown-menu-end shadow-lg border-0 rounded-4 mt-2"
+        aria-labelledby="dropdownMenuButton"
+      >
+        <li>
+          <a className="dropdown-item py-2" href="#">
+            👤 Mi perfil
+          </a>
+        </li>
+
+        <li>
+          <a className="dropdown-item py-2" href="#">
+            ✉️ Contáctanos
+          </a>
+        </li>
+
+        <li>
+          <a className="dropdown-item py-2" href="#">
+            ⚙️ Configuración
+          </a>
+        </li>
+      </ul>
     </div>
-    );
+  );
 }
 
 export default MenuDesplegable;
