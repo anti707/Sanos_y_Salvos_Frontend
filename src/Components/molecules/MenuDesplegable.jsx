2270ed0a8da2d56bc9ebd36ea2f23846e609c980
anti707 <ant.tapiao@duocuc.cl>
2026-05-15 16:50:48 -0400

    commit menu desplegable

--- src/Components/molecules/MenuDesplegable.jsx
+++ src/Components/molecules/MenuDesplegable.jsx
@@ -1,6 +1,6 @@
 
 
-function MenuDesplegable({ mascota }) {
+function MenuDesplegable{
   return (
    <div class="dropdown">
   <a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
@@ -8,9 +8,9 @@ function MenuDesplegable({ mascota }) {
   </a>
 
   <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-    <li><a class="dropdown-item" href="#">Action</a></li>
-    <li><a class="dropdown-item" href="#">Another action</a></li>
-    <li><a class="dropdown-item" href="#">Something else here</a></li>
+    <li><a class="dropdown-item" href="#">Mi perfil</a></li>
+    <li><a class="dropdown-item" href="#">Contactanos</a></li>
+    <li><a class="dropdown-item" href="#">Configuracion</a></li>
   </ul>
 </div>
   );
