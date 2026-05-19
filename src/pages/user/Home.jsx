e748b4a6b8887b4cce4371487e249b2d45121d09
anti707 <ant.tapiao@duocuc.cl>
2026-05-18 21:04:20 -0400

    cards

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import { Container } from 'react-bootstrap';
 import "../../css/pages/Home.css";
+import "../../Components/molecules/CardHorizontal"
+
 
 
 
 function Home() {
-    const [mascotas] = useState([]);
 
  
   return (
@@ -22,31 +23,9 @@ function Home() {
         </p>
       </div>
 
-      <div className="mascotas-home-wrapper">
 
-        <div className="mascotas-home-list">
-          {mascotas.map((mascota) => (
-            <div key={mascota.mascota_id} className="home-card">
+      
 
-              <img src={mascota.imagenUrl} alt={mascota.nombreMascota} className="home-card-img" />
-
-              <h3 className="home-card-title">{mascota.nombreMascota}</h3>
-
-              <p className="home-card-desc">
-                {mascota.descripcion?.slice(0, 160)}
-              </p>
-
-              <a className="home-card-btn" href={`/mascota/${mascota.mascota_id}`}>
-                Ir a detalles
-              </a>
-
-            </div>
-          ))}
-        </div>
-            
-
-
-      </div>
 
     </Container>
   );
