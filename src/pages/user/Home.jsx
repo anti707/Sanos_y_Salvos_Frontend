84bf931fcad4b37471ecb4fde00ca98e7ceb1fa0
anti707 <ant.tapiao@duocuc.cl>
2026-05-18 21:39:37 -0400

    cartas añadidas

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Container } from 'react-bootstrap';
 import "../../css/pages/Home.css";
 import "../../Components/molecules/CardHorizontal"
+import CardHorizontal from "../../Components/molecules/CardHorizontal";
 
 
 
@@ -17,13 +18,9 @@ function Home() {
         <h1 className="home-title">Sanos y Salvos</h1>
       </div>
 
-      <div className="home-box">
-        <p className="home-text">
-          <strong>Sanos y Salvos</strong> somos un equipo sin fines de lucro dedicados a la busqueda y encuentro de mascotas perdidas.
-        </p>
-      </div>
-
-
+      <CardHorizontal/>
+      <CardHorizontal/>
+      <CardHorizontal/>
       
 
 
