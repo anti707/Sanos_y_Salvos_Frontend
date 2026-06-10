104a8756adced8e3ee5fcb7835b1b8cc17b6b1fc
anti707 <ant.tapiao@duocuc.cl>
2026-06-10 15:54:44 -0400

    detalles pagina webpay

--- src/Components/molecules/ApoyanosCard.jsx
+++ src/Components/molecules/ApoyanosCard.jsx
@@ -2,38 +2,42 @@ import "../../css/Components/molecules/ApoyanosCard.css";
 
 function ApoyanosCard() {
   return (
-    <div className="apoyanos-container">
-      
-      <div className="card border-info mb-3" style={{ maxWidth: "18rem" }}>
+    <div className="apoyanos-container"> 
+
+ 
+      <div className="card border-info custom-card mb-3" style={{ maxWidth: "18rem" }}>
         <div className="card-header">Header</div>
         <div className="card-body">
-          <h5 className="card-title">Primary card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        </div>
-      </div>
-      <div className="card border-info mb-3" style={{ maxWidth: "18rem" }}>
-        <div className="card-header">Header</div>
-        <div className="card-body">
-          <h5 className="card-title">Primary card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        </div>
-      </div>
-      <div className="card border-info mb-3" style={{ maxWidth: "18rem" }}>
-        <div className="card-header">Header</div>
-        <div className="card-body">
-          <h5 className="card-title">Primary card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-        </div>
-      </div>
-      <div className="card border-info mb-3" style={{ maxWidth: "18rem" }}>
-        <div className="card-header">Header</div>
-        <div className="card-body">
-          <h5 className="card-title">Primary card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+          <h5 className="className">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         </div>
       </div>
 
-    </div>
+      <div className="card border-info custom-card mb-3" style={{ maxWidth: "18rem" }}>
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div className="card border-info custom-card mb-3" style={{ maxWidth: "18rem" }}>
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+      <div className="card border-info custom-card mb-3" style={{ maxWidth: "18rem" }}>
+        <div className="card-header">Header</div>
+        <div className="card-body">
+          <h5 className="card-title">Primary card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+
+    </div> 
   );
 }
 
--- src/pages/user/Apoyanos.jsx
+++ src/pages/user/Apoyanos.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Container } from 'react-bootstrap';
 import "../../css/pages/Apoyanos.css";
 import ApoyanosCard from "../../Components/molecules/ApoyanosCard";
+import { Link } from 'react-router-dom';
 
 
 function Apoyanos() {
@@ -26,6 +27,38 @@ function Apoyanos() {
         </div>
       </div>
 
+
+      <div className="apoyanos-nosotros">
+        <h1 className="apoyanos-title">¡Quiero apoyar!</h1>
+        <div className="apoyanos-boton">
+          <Link to="/contactanos" className="btn-primary">
+            Contactanos
+          </Link>
+        </div>
+      </div>
+
+      <div className="my-5" style={{width: "100vw",marginLeft: "calc(-50vw + 50%)",}}>
+          <img
+            src="https://i.pinimg.com/originals/27/19/12/271912b329ec663f31a95238cae5a6fd.gif"
+            alt="Banner mascotas"
+            className="w-80 rounded"
+            style={{
+              height: "250px",
+              objectFit: "scale-down",
+            }}
+          />
+        </div> 
+
+
+      <div className="apoyanos-nosotros">
+        <h1 className="apoyanos-title">Sobre nosotros</h1>
+        <h2 className="apoyanos-subtitle">¿Quieres saber mas de nosotros?, contactanos a traves de nuestras redes y/o sucursales</h2>
+        <div className="apoyanos-boton">
+          <Link to="/contactanos" className="btn-primary">
+            Contactanos
+          </Link>
+        </div>
+      </div>
 
     </Container>
   );
